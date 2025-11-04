4bbae1e06f9e891f8c4a6c1affd52b75356d7eee
Daniel de Oliveira Rodrigues <danieloliveirarodrigues@hotmail.com>
2025-11-03 21:36:22 -0300

    Delete Pagamento interface
    Removed Pagamento interface and its methods.

--- Pagamento.java
+++ Pagamento.java
@@ -1,6 +1,1 @@
-package br.com.aluno.pagamento;
 
-public interface Pagamento {
-    double calcularValorFinal(double valor);
-    void processarPagamento(double valor) throws PagamentoInvalidoException;
-}
